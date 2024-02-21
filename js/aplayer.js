a323a1a518729fe52e7973a3105747c9e59468b3
IdealDestructor <wdqjj@qq.com>
2024-02-21 06:03:56 +0000

    Site updated: 2024-02-21 06:03:55

---
+++ js/aplayer.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    lrcType: 3,
+    audio: [
+        {
+            name: 'I Really Want to Stay At Your House.mp3',
+            artist: 'Rosa Walton/Hallie Coggins',
+            url: 'https://deqiang.wang/IReallyWanttoStayAtYourHouse.mp3',
+            cover: 'https://deqiang.wang/IReallyWanttoStayAtYourHouse.jpg',
+            lrc: 'https://deqiang.wang/IReallyWanttoStayAtYourHouse.lrc'
+        }
+    ]
+});
